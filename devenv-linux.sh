7afe9c434671bcce16b8d83da9647162f2ada7df
Romain Boman <romain.boman@gmail.com>
2019-02-02 16:39:14 +0100

    add LD_LIBRARY_PATH for macOS runtime

--- devenv-linux.sh
+++ devenv-linux.sh
@@ -5,5 +5,6 @@ export PATH=${GMSHSDK}/bin:${GMSHSDK}/lib:${PATH}
 export INCLUDE=${GMSHSDK}/include:${INCLUDE}
 export LIB=${GMSHSDK}/lib:${LIB}
 export PYTHONPATH=${GMSHSDK}/lib:${PYTHONPATH}
+export DYLD_LIBRARY_PATH=${GMSHSDK}/lib:${DYLD_LIBRARY_PATH}
 
 # examples in ${GMSHSDK}/share/doc/gmsh/demos/api/
