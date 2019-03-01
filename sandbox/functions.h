afc6fc1fa1052bdcfeaf5435b553ef913ad54fbf
GSchnackers <gaudericschnackers@hotmail.com>
2019-03-01 13:39:32 +0100

    Comment in function.h

--- sandbox/functions.h
+++ sandbox/functions.h
@@ -1,7 +1,10 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+// Sorting the nodes of the list of the edges to remove duplicate
 void mysorting(std::vector<int> & nodes);
+
+// Return the normals at each edges
 std::vector<double> my2Dnormal(std::vector<int> nodes);
 
 #endif
