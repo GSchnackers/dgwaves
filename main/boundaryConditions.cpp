9a2ea08f64ed408caf0808b7342b272a26090a1d
GSchnackers <gaudericschnackers@hotmail.com>
2019-03-26 16:48:33 +0100

    boundary conditions in the domain

--- main/boundaryConditions.cpp
+++ main/boundaryConditions.cpp
@@ -9,7 +9,7 @@ void boundaryConditions(std::vector<double> & coord,double mytime, double & valu
     //double T = 2; //longueur d'onde
     //value=sin(2*M_PI*mytime/T);
     if(coord[0] == 0)
-        value = 0.1;
+        value = 0;
     else
         value = 0;
 }
--- main/initialCondition.cpp
+++ main/initialCondition.cpp
@@ -8,7 +8,11 @@ void initialCondition(std::vector<double> & coord, double & value){
     
     //double L=0.25; //longueur d'onde
     //double R = sqrt(coord[0]*coord[0]+coord[1]*coord[1]);
-    value=0;
+    //value=0;
     //value = sin(2*M_PI*R/L)/R; // vague comme quand une goute tombe dans l'eau
 
+    if(coord[0] == 0)
+        value = 0.1;
+    else
+        value = 0;
 }
