d5452747b57f5761b70cccc40d110266d8898940
GSchnackers <gaudericschnackers@hotmail.com>
2019-03-26 18:26:16 +0100

    change init cond

--- main/initialCondition.cpp
+++ main/initialCondition.cpp
@@ -10,9 +10,4 @@ void initialCondition(std::vector<double> & coord, double & value){
     //double R = sqrt(coord[0]*coord[0]+coord[1]*coord[1]);
     value = 1;
     //value = sin(2*M_PI*R/L)/R; // vague comme quand une goute tombe dans l'eau
-
-    if(coord[0] == 0)
-        value = 0.1;
-    else
-        value = 0;
 }
