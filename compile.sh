48d95f8c21a031d8afe707d3cabe9dfd6f0b36f6
Nicolas <nicolascorthouts@gmail.com>
2019-05-20 23:59:12 +0200

    Compilation on NIC4

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+module load cmake/3.11.1
+module load gcc/4.9.2
+
+export LD_LIBRARY_PATH=dgwaves/gmsh-sdk/lib
+export FC=gfortran
+
+rm -rf build/  
+mkdir build
+
+cd build/
+cmake .. 
+make
+
+cd ..
