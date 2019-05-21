23297f6edf7100beafdcb0147c1af759d74a0cdc
nicocorth <47564166+nicocorth@users.noreply.github.com>
2019-05-21 02:31:48 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-module load cmake/3.11.1
-module load gcc/4.9.2
+  module load cmake/3.11.1
+  module load gcc/4.9.2
 
-export LD_LIBRARY_PATH=gmsh-sdk/lib
+export CC=gcc
+export CXX=g++
+
+cd gmsh-sdk/
 export FC=gfortran
+export PATH=${PWD}/bin:${PWD}/lib:${PATH}
+export INCLUDE=${PWD}/include:${INCLUDE}
+export LIB=${PWD}/lib:${LIB}
+export PYTHONPATH=${PWD}/lib:${PYTHONPATH}
+cd ../
 
-rm -rf build/  
+cd eigen/
+export INCLUDE=${PWD}:${INCLUDE}
+
+cd ../
+rm -rf build/
 mkdir build
 
 cd build/
-cmake .. 
+cmake ..
 make
-
-cd ..
